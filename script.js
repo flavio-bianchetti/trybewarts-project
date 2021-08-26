d3148c7aa70f0283476c6298664c7ec9312bfd89
Flávio Bianchetti <fjb.developer@gmail.com>
2021-08-26 16:42:12 -0300

    Alteraçoes apos atualizaçao da branch main

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
 const btn = document.getElementById('buttonLogin');
+const submitBtn = document.getElementById('submit-btn');
+const agreement = document.getElementById('agreement');
+
 function testeBtn() {
   if (input1.value === 'tryber@teste.com' && input2.value === '123456') {
     alert('Olá, Tryber!');
@@ -13,21 +16,19 @@ function btnLogin() {
   btn.addEventListener('click', testeBtn);
 }
 
+function checkBoxStatus() {
+  if (agreement.checked) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+}
+
+function inputCheckBox() {
+  agreement.addEventListener('click', checkBoxStatus);
+}
+
 window.onload = function pageOnload() {
   btnLogin();
+  inputCheckBox();
 };
-
-// const submitBtn = document.getElementById('submit-btn');
-// const agreement = document.getElementById('agreement');
-
-// function checkBoxStatus() {
-//   if (agreement.checked) {
-//     submitBtn.enabled = true;
-//   } else {
-//     submitBtn.enabled = false;
-//   }
-// }
-
-// function inputCheckBox() {
-// agreement.addEventListener('click', checkBoxStatus);
-// }
